6cf5c3b6660a60fb7aed82f05e23ffa5f9595be5
Stormbreaker20 <luvsharma.del@gmail.com>
2024-12-17 09:44:44 +0530

    ready to host

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
-import {proxy} from 'valtio'
+import { proxy } from "valtio";
 
 export const state = proxy({
-    delay:500,
-    ArraySize:20,
-    AlgoSelected:null,
-    isRunning:false
-})
+  delay: 500,
+  ArraySize: 20,
+  AlgoSelected: "Regression",
+  isRunning: false,
+});
